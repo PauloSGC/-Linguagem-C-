4918d03a03a38addb83f594ff5159cf8bb1937a3
Paulo <124412687+PauloSGC@users.noreply.github.com>
2024-04-21 21:01:06 -0700

    Atualizar o Função Hipotenusa de um Triângulo.cpp

--- Code in C/Função Hipotenusa de um Triângulo.cpp
+++ Code in C/Função Hipotenusa de um Triângulo.cpp
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
-//FunÁ„o Hipotenusa de um Tri‚ngulo
-	float hipotenusa (float cateto1, float cateto2){//Declarar o tipo da minha funÁ„o "flaot", e as vari·veis tambÈm.
-	return sqrt(cateto1 * cateto1 + cateto2 * cateto2);//ApÛs o dado recebido, retornar· e efetuar· a operaÁ„o.
+//Fun√ß√£o Hipotenusa de um Tri√¢ngulo
+	float hipotenusa (float cateto1, float cateto2)//Declarar o tipo da minha fun√ß√£o "flaot", e as vari√°veis tamb√©m.
+    {
+	return sqrt(cateto1 * cateto1 + cateto2 * cateto2);//Ap√≥s o dado recebido, retornar√° e efetuar√° a opera√ß√£o.
 	}
 	int main(){
-		setlocale (LC_ALL, "Portuguese");//biblioteca que permite a acentuaÁ„o dos caracteres.
-		float cateto1, cateto2, resultado;//DeclaraÁ„o das vari·veis para receber os dados.
+		setlocale (LC_ALL, "Portuguese");//biblioteca que permite a acentua√ß√£o dos caracteres.
+		float cateto1, cateto2, resultado;//Declara√ß√£o das vari√°veis para receber os dados.
 			printf("Digite o cateto oposto: ");
 			scanf("%f", &cateto1);
 			printf("Digite o cateto adjacente: ");
 			scanf("%f", &cateto2);
-			resultado = hipotenusa(cateto1, cateto2);//Armazena a funÁ„o na vari·vel "resultado".
-			printf("A hipotenusa do tri‚ngulo È: %2.f\n", resultado);
+			resultado = hipotenusa(cateto1, cateto2);//Armazena a fun√ß√£o na vari√°vel "resultado".
+			printf("A hipotenusa do tri√¢ngulo √©: %2.f\n", resultado);
 	return 0;		
 	}
