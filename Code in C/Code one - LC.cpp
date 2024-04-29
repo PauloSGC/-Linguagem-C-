38838ac53dd7b068d43cb3616b14a92852c79a43
Paulo <124412687+PauloSGC@users.noreply.github.com>
2024-04-28 23:08:29 -0400

    Update Code one - LC.cpp

--- Code in C/Code one - LC.cpp
+++ Code in C/Code one - LC.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
-         int main(){
-             printf("Hello, World!");
+int main()
+{        
+         printf("Hello, World!");
+         printf("My name is Paulo, and studding information system");
          return 0;    
 }
