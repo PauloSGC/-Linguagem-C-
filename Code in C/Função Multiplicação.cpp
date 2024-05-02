1ed2569a6dc6aa614c00803e2a8c8d0c7fdb42a4
Paulo <124412687+PauloSGC@users.noreply.github.com>
2024-05-01 23:32:46 -0400

    Update Função Multiplicação.cpp

--- Code in C/Função Multiplicação.cpp
+++ Code in C/Função Multiplicação.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <locale.h>
-//Função Multiplicação
+//FunÃ§Ã£o MultiplicaÃ§Ã£o
          int mult(int a, int b){
          return a*b;
          }
-         int main(){
-         	 int num1, num2, resultado;
-             setlocale(LC_ALL, "Portuguese");//Biblioteca para a acentuação de caracteres...
-             printf("insira o primeiro número: ");
+         int main()
+         {
+             int num1, num2, resultado;
+             setlocale(LC_ALL, "Portuguese");//Biblioteca para a acentuaÃ§Ã£o de caracteres...
+                  printf("insira o primeiro nÃºmero: ");
                     scanf("%d", &num1);
-             printf("insira o segundo número: ");
+                   printf("insira o segundo nÃºmero: ");
                     scanf("%d", &num2);
-             resultado = mult(num1, num2);//Chamar a função...
-             printf("O resultado da multiplicação desses números é %d\n", resultado);
+                           resultado = mult(num1, num2);//Chamar a funÃ§Ã£o...
+                  printf("O resultado da multiplicaÃ§Ã£o desses nÃºmeros Ã© %d\n", resultado);
          return 0;                           
          }         
