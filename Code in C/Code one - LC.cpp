ee47333f5454ca79eebeb830dc80e3ea7520cd84
Paulo <124412687+PauloSGC@users.noreply.github.com>
2024-05-06 00:15:27 -0400

    Update Code one - LC.cpp

--- Code in C/Code one - LC.cpp
+++ Code in C/Code one - LC.cpp
@@ -4,6 +4,7 @@ int main()
          printf("Hello, World!");
          printf("My name is Paulo, and studding information system");
          printf("I am 24 years old");
+
          
          return 0;    
 }
